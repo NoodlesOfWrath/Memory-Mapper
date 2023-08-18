2554820392cc5e2220a3af43788fe83b0ccf8174
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-08-17 19:48:29 -0500

    Added thumbnail caching
    Heavily increased performance by adding thumbnail caching

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Ellipse
 from kivy.core.window import Window
 from kivy.clock import Clock
-import random
 import math
 import os
 import kivy
-import PIL
-from PIL import Image
 from kivy.core.image import Image as CoreImage
-from kivy.uix.image import Image as kiImage
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from io import BytesIO
 
 
@@ -33,27 +27,43 @@ def image_to_texture(img):
     return im.texture
 
 
+def generate_image_previews():
+    thumbnail_size = (128, 128)
+    thumbnail_folder = os.path.dirname(os.path.abspath(__file__)) + "//Memory_Thumbnails"
+    full_res_images_dir = os.path.dirname(os.path.abspath(__file__)) + "//Full_Res_Memories"
+
+    for image_name in os.listdir(full_res_images_dir):
+        thumbnail_file_dir = thumbnail_folder + "//" + image_name
+        # Check if the image already has a cached thumbnail
+        if not os.path.isfile(thumbnail_file_dir):
+            image = Image.open(full_res_images_dir + "//" + image_name)
+            image.thumbnail(thumbnail_size, Image.Resampling.LANCZOS)
+            image.save(thumbnail_file_dir, "JPEG")
+
+
 class CircleWidget(Widget):
 
-    def __init__(self, text='', label_color=(0, 0, 0, 1), circle_color=(0, 0, 0, 0), circle_size=1, circle_image=None, **kwargs):
+    def __init__(self, text='', label_color=(0, 0, 0, 1), circle_color=(0, 0, 0, 0), circle_size=1, circle_image=None,
+                 **kwargs):
         super(CircleWidget, self).__init__(**kwargs)
 
         # Define circle_size as an instance variable
         self.circle_size = circle_size
 
         with self.canvas:
-            if circle_image == None:
+            if circle_image is None:
                 # Set the position and size of the circle here
-                self.circle = Ellipse(pos=self.pos, size=(50*circle_size, 50*circle_size), color=circle_color)
+                self.circle = Ellipse(pos=self.pos, size=(50 * circle_size, 50 * circle_size), color=circle_color)
             else:
-                self.circle = Ellipse(pos=self.pos, size=(50 * circle_size, 50 * circle_size), color=circle_color, texture=circle_image)
+                self.circle = Ellipse(pos=self.pos, size=(50 * circle_size, 50 * circle_size), color=circle_color,
+                                      texture=circle_image)
 
         self.label = Label(text=text, halign='center', valign='middle', font_size=40, color=label_color)
         self.add_widget(self.label)
 
     def on_size(self, *args):
         circle_size = min(self.width, self.height)
-        circle_size = float(circle_size)*float(self.circle_size)
+        circle_size = float(circle_size) * float(self.circle_size)
         self.circle.pos = (self.center_x - circle_size / 2, self.center_y - circle_size / 2)
         self.circle.size = (circle_size, circle_size)
 
@@ -73,6 +83,8 @@ class TheLabApp(App):
         self.mos_pos = ()
         self.memory_count = 240
 
+        # Generate previews for all memories and cache them (if not done already)
+        generate_image_previews()
         self.update_circles()
 
         Clock.schedule_interval(self.update_mouse_pos, 0.1)
@@ -84,17 +96,17 @@ class TheLabApp(App):
             # Get the mouse position using Kivy's Window module
             self.mos_pos = Window.mouse_pos
             # Adjust for pixel density
-            self.mos_pos = (self.mos_pos[0]*kivy.metrics.dp(1), self.mos_pos[1]*kivy.metrics.dp(1))
+            self.mos_pos = (self.mos_pos[0] * kivy.metrics.dp(1), self.mos_pos[1] * kivy.metrics.dp(1))
             self.set_circle_scale(self.main_layout)
 
     def update_circles(self):
         self.main_layout.clear_widgets()
 
-        full_res_images_dir = os.path.dirname(os.path.abspath(__file__)) + "//Full_Res_Memories"
+        memory_thumbnail_folder = os.path.dirname(os.path.abspath(__file__)) + "//Memory_Thumbnails"
 
         for i in range(self.memory_count):
-            if len(os.listdir(full_res_images_dir)) > i:
-                image = Image.open(full_res_images_dir + "//" + os.listdir(full_res_images_dir)[i])
+            if len(os.listdir(memory_thumbnail_folder)) > i:
+                image = Image.open(memory_thumbnail_folder + "//" + os.listdir(memory_thumbnail_folder)[i])
                 image = image_to_texture(image)
             else:
                 print("no images found...")
@@ -110,12 +122,12 @@ class TheLabApp(App):
 
             distance_to_circle = math.dist(widget_pos, self.mos_pos)
 
-            window_scaling_factor = Window.height+Window.width
+            window_scaling_factor = Window.height + Window.width
 
             # Ensure the input value is within the specified range
-            distance_to_circle = max(window_scaling_factor/100, min(distance_to_circle, window_scaling_factor/2))
+            distance_to_circle = max(window_scaling_factor / 100, min(distance_to_circle, window_scaling_factor / 2))
             distance_to_circle /= window_scaling_factor
-            radius = 1/distance_to_circle
+            radius = 1 / distance_to_circle
             radius *= 0.02
             circle.update_circle_size(radius)
             i += 1
